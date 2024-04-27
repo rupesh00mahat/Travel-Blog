f9ec5468e428b695a298a7c83c4e6f28f892f933
rupesh00mahat <rupesh00mahat@gmail.com>
2024-04-27 20:28:35 +0545

    fix responsive issue navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./navbar";
 import "../style.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+
 
 import SliderComponent from "./slider-component";
 import "swiper/css/navigation";
@@ -12,9 +15,34 @@ import "swiper/css/scrollbar";
 import SwiperComponent from "./swiper-component";
 
 function Header(props) {
+  const [toggleState, changeToggleState] = useState(false);
+
+  const toggleNavbar = (value)=>{
+changeToggleState(value);
+if(value === true){
+  const navbar = document.querySelector(`nav`);
+ navbar.style.display = 'flex';
+}
+else if(value === false){
+  const navbar = document.querySelector(`nav`);
+ navbar.style.display = 'none';
+}
+  }
+
   return (
     <div id="header">
       {props.hasNavbar && <Navbar />}
+      <div className="hamburger-menu">
+      {toggleState == false && <GiHamburgerMenu  onClick={()=>{
+        toggleNavbar(true);
+      }}/>}
+     {toggleState == true && <IoClose onClick={()=>{
+                toggleNavbar(false);
+
+      }}/>}
+
+        </div>
+
       <SwiperComponent
       firstTitle ="travel blog"
       firstContent='The principle of perception reflects a typical sign. Hence it follows naturally that bhatavada makes a sign. Common sense, of course, is ambiguous, According to the opinion of well-lnown philosophers, the pain decomposes structuralism into elements. Structuralism mentally takes into account the deductive method.'
